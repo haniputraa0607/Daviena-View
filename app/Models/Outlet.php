914a828ae6dbaa36f50fc353d5f71ef5e0eda5c4
hendrytime2beat <hendrytime2beat@gmail.com>
2023-11-10 14:07:46 +0700

    update jumat last day

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use KodePandai\Indonesia\Models\District;
 
 // use Modules\Outlet\Entities\OutletSchedule;
@@ -14,6 +15,7 @@ use KodePandai\Indonesia\Models\District;
 class Outlet extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'outlets';
     protected $fillable = [
